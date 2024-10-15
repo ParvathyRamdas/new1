9379e98865a3190edd631e2587a80f2d9d593103
parvathy v <164923298+ParvathyRamdas@users.noreply.github.com>
2024-10-15 23:49:32 +0530

    Add files via upload

---
+++ cardemo.java
@@ -0,0 +1,14 @@
+class car{
+   String brand;
+   int year;
+   double mileage;
+}
+public class cardemo{
+   public static void main(String args[]){
+     car c1=new car();
+     c1.brand="Toyota";
+     c1.year=2020;
+     c1.mileage=16.6;
+     System.out.println("brand"+c1.brand);
+}}
+     
